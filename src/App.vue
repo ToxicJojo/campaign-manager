e805e16a405d80ae2345782951a0ecac05cd821b
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-07-17 11:45:54 +0200

    Removed unused files created by the cli

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <template lang='pug'>
   v-app
     v-toolbar(app='')
-      v-toolbar-title.headline.text-uppercase
-        span Vuetify
-        span.font-weight-light MATERIAL DESIGN
-      v-spacer
-      v-btn(flat='', href='https://github.com/vuetifyjs/vuetify/releases/latest', target='_blank')
-        span.mr-2 Latest Release
     v-content
-      HelloWorld
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
-  data () {
-    return {
-      //
-    }
-  },
 }
 </script>
