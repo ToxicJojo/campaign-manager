16a7b5b5011e6a5ea4976b1c03d4e205628780c0
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-07-17 11:33:43 +0200

    Updated eslintrc to match coding style
    Added the multiline comma dangle rule

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,12 @@ import HelloWorld from './components/HelloWorld'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   },
   data () {
     return {
       //
     }
-  }
+  },
 }
 </script>
