4865c7176311fdb90b44f3481881ec4d2c2e0363
Johannes Thiel <Johannes.Thiel@uni-duesseldorf.de>
2019-07-24 14:31:22 +0200

    Object store skeleton

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 
 import state from './state'
 import campaigns from './campaigns'
+import objects from './objects'
 
 Vue.use(Vuex)
 
@@ -10,6 +11,7 @@ const store = new Vuex.Store({
   state,
   modules: {
     campaigns,
+    objects,
   },
 })
 
